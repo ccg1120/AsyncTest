9bdee9548f0e85ccb2b43b7437128bc98263567d
ccg1120 <ccg1120@naver.com>
2017-10-17 22:42:14 +0900

    commit

---
+++ Assets/Scripts/MultiCoreTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using System;
+using System.Threading.Tasks;
+
+public class MultiCoreTest : MonoBehaviour {
+
+    static void TaskMethod(string name)
+    {
+        
+        Debug.Log("Task TEST "+ name);
+        Debug.Log("thread id :: "+ Thread.CurrentThread.ManagedThreadId);
+        Debug.Log("poolthread :: " + Thread.CurrentThread.IsThreadPoolThread);
+    }
+	// Use this for initialization
+	void Start () {
+        Task t1 = new Task(() => TaskMethod("t1"));
+        Task t2 = new Task(() => TaskMethod("t2"));
+
+        t1.Start();
+        t2.Start();
+
+        Task.Run(() => TaskMethod("t3"));
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+}
