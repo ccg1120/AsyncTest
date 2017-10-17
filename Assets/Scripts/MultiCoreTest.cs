05d6797422366c73a9d1f943b9af249169d36b5c
ccg1120 <ccg1120@naver.com>
2017-10-18 02:28:55 +0900

    task 를 이용한 cpu 사용 및 속도 비교

--- Assets/Scripts/MultiCoreTest.cs
+++ Assets/Scripts/MultiCoreTest.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 using System.Threading;
 using System;
 using System.Threading.Tasks;
+using System.IO;
+
 
 public class MultiCoreTest : MonoBehaviour {
+
+    string pathfolder = "Data";
+    string filename = "IMG.jpg";
+    public SpriteRenderer sr;
+
+    public static Texture2D[] tts = new Texture2D[10];
 
     static void TaskMethod(string name)
     {
@@ -16,15 +24,94 @@ public class MultiCoreTest : MonoBehaviour {
     }
 	// Use this for initialization
 	void Start () {
-        Task t1 = new Task(() => TaskMethod("t1"));
-        Task t2 = new Task(() => TaskMethod("t2"));
+        for(int i = 0; i < 10; i++)
+        {
+            tts[i] = new Texture2D(1, 1);
+        }
+
+        //Task t1 = new Task(() => TaskMethod("t1"));
+        //Task t2 = new Task(() => TaskMethod("t2"));
+
+        //t1.Start();
+        //t2.Start();
+
+        //Task.Run(() => TaskMethod("t3"));
+        //Thread.Sleep(TimeSpan.FromSeconds(1));
+
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Reset();
+        sw.Start();
+
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    string filename = "IMG" + i.ToString() + ".jpg";
+        //    ReadImage(filename,i);
+        //}
+
+        //Task t1 = new Task(() => ReadImage("IMG1.jpg", 0));
+        //Task t2 = new Task(() => ReadImage("IMG2.jpg", 1));
+        //Task t3 = new Task(() => ReadImage("IMG3.jpg", 2));
+        //Task t4 = new Task(() => ReadImage("IMG4.jpg", 3));
+        //Task t5 = new Task(() => ReadImage("IMG5.jpg", 4));
+
+        //Task t6 = new Task(() => ReadImage("IMG6.jpg", 5));
+        //Task t7 = new Task(() => ReadImage("IMG7.jpg", 6));
+        //Task t8 = new Task(() => ReadImage("IMG8.jpg", 7));
+        //Task t9 = new Task(() => ReadImage("IMG9.jpg", 8));
+        //Task t10 = new Task(() => ReadImage("IMG10.jpg", 9));
+
+
+        Task t1 = new Task(() => IntAddMax());
+        Task t2 = new Task(() => IntAddMax());
+        Task t3 = new Task(() => IntAddMax());
+        Task t4 = new Task(() => IntAddMax());
+        Task t5 = new Task(() => IntAddMax());
+
+        Task t6 = new Task(() => IntAddMax());
+        Task t7 = new Task(() => IntAddMax());
+        Task t8 = new Task(() => IntAddMax());
+        Task t9 = new Task(() => IntAddMax());
+        Task t10 = new Task(() => IntAddMax());
+
+        //Task<UInt32> t1 = IntAddMaxasync();
+        //Task<UInt32> t2 = IntAddMaxasync();
+        //Task<UInt32> t3 = IntAddMaxasync();
+        //Task<UInt32> t4 = IntAddMaxasync();
+        //Task<UInt32> t5 = IntAddMaxasync();
+        //Task<UInt32> t6 = IntAddMaxasync();
+        //Task<UInt32> t7 = IntAddMaxasync();
+        //Task<UInt32> t8 = IntAddMaxasync();
+        //Task<UInt32> t9 = IntAddMaxasync();
+        //Task<UInt32> t10 = IntAddMaxasync();
 
         t1.Start();
         t2.Start();
+        t3.Start();
+        t4.Start();
+        t5.Start();
+        t6.Start();
+        t7.Start();
+        t8.Start();
+        t9.Start();
+        t10.Start();
+        
+        t1.Wait();
+        t2.Wait();
+        t3.Wait();
+        t4.Wait();
+        t5.Wait();
+        t6.Wait();
+        t7.Wait();
+        t8.Wait();
+        t9.Wait();
+        t10.Wait();
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    IntAddMax();
+        //}
 
-        Task.Run(() => TaskMethod("t3"));
-        Thread.Sleep(TimeSpan.FromSeconds(1));
-
+        sw.Stop();
+        Debug.Log(sw.Elapsed.ToString());
     }
 	
 	// Update is called once per frame
@@ -32,4 +119,53 @@ public class MultiCoreTest : MonoBehaviour {
 		
 	}
     
+    public void ReadImage(string filename, int num)
+    {
+    
+        //Debug.Log(Application.persistentDataPath);
+        byte[] bytes = File.ReadAllBytes(@"E:\Study\UnityTask\Assets\Data\"+filename);
+        
+        //Debug.Log(bytes.Length);
+        //Sprite sp = new Sprite();
+        
+        
+        //tts[num].LoadImage(bytes);
+        //tts[num].Apply();
+        //Debug.Log("texture width :: " + tts[num].width  + " :: texture height ::" + tts[num].height);
+        //sp = Sprite.Create(tts[num], new Rect(0,0, tts[num].width, tts[num].height),Vector2.zero);
+        //sr.sprite = sp;
+        
+    }
+    public void  IntAddMax()
+    {
+        UInt32 num = 0;
+        for(int i = 0; i < int.MaxValue; i++)
+        {
+            num = num + (UInt32)i;
+        }
+    }
+
+    async static Task<UInt32> IntAddMaxasync()
+    {
+        UInt32 num = 0;
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            num = num + (UInt32)i;
+        }
+        await Task.FromResult<UInt32>(num);
+        return num;
+    }
+    //static Task taskall()
+    //{
+    //    Task<UInt32> t1 = IntAddMaxasync();
+    //    Task<UInt32> t2 = IntAddMaxasync();
+    //    Task<UInt32> t3 = IntAddMaxasync();
+    //    Task<UInt32> t4 = IntAddMaxasync();
+    //    Task<UInt32> t5 = IntAddMaxasync();
+    //    Task<UInt32> t6 = IntAddMaxasync();
+    //    Task<UInt32> t7 = IntAddMaxasync();
+    //    Task<UInt32> t8 = IntAddMaxasync();
+    //    Task<UInt32> t9 = IntAddMaxasync();
+    //    Task<UInt32> t10 = IntAddMaxasync();
+    //}
 }
