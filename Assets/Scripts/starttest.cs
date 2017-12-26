fceb7d8c36ff46932bf5ecc7eb969114ee5f8227
ccg1120 <ccg1120@daum.net>
2017-12-26 10:07:22 +0900

    commit

---
+++ Assets/Scripts/starttest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starttest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        uint total = 0;
+
+		for(int i = 0; i < 1000; i++)
+        {
+            total = total + (uint)i;
+            Debug.Log(this.gameObject.name + " :: " + i);
+        }
+
+	}
+	
+	
+}
