110fdd4c75c1691e03778a3de4b4a1c34b8dae73
ccg1120 <ccg.1120@daum.net>
2018-01-16 01:33:58 +0900

    메인에서는 코루틴 호출을 비동기 하게 호출 하려고 했으나 되지 않음 좀더알아볼것

--- Assets/Scripts/ImageRead.cs
+++ Assets/Scripts/ImageRead.cs
@@ -16,10 +16,13 @@ public class ImageRead : MonoBehaviour {
 
     [SerializeField]
     public List<byte[]> bytesList;
-    
+    public bool[] ResultInArray = new bool[10];
+    public bool[] ResultOutArray = new bool[10];
+
+    public int count = 0; 
     private void Start()
     {
-        
+        UnityEngine.Debug.Log("Main Init!!");
         images = new Texture2D[10];
         bytesList = new List<byte[]>();
         Path = Application.dataPath + "/Data/IMG";
@@ -27,13 +30,60 @@ public class ImageRead : MonoBehaviour {
      
         for (int index =0; index < 10; index++)
         {
-            //CreateImage(Path + (index + 1).ToString() + SubPath,index);
-            StartCoroutine(ICreateImage(RoadImage(Path + (index + 1).ToString() + SubPath), index));
+            CreateImage(Path + (index + 1).ToString() + SubPath,index);
+            //StartCoroutine(ICreateImage(RoadImage(Path + (index + 1).ToString() + SubPath), index));
         }
 
+        //for (int index = 0; count < 10; index++)
+        //{
+        //    UnityEngine.Debug.Log("index ::" + index);
+            
+        //    if (ResultInArray[index] == true)
+        //    {
+        //        count++;
+        //        UnityEngine.Debug.Log("count     ::" + count);
+        //    }
+        //    if (index >= 9) index = 0;
+        //    //if (ResultOutArray[index] && ResultInArray[index])s
+        //    //{
+        //    //    count++;
+        //    //    if (count == 10) break;
+        //    //}
+        //    //else if (!ResultOutArray[index] && ResultInArray[index])
+        //    //{
+        //    //    //StartCoroutine(ICreateImage(bytesList[index], index));
+        //    //}
+        //}
         UnityEngine.Debug.Log("Main END!!"); //모든 비동기 메소드를 생성 시키고 종료 됨 
     }
 
+
+    private void Update()
+    {
+        
+        if(count <= 9)
+        {
+            UnityEngine.Debug.Log("Main Update!!");
+            for (int index = 0; index < 10; index++)
+            {
+                UnityEngine.Debug.Log("index ::" + index);
+
+                if (ResultOutArray[index] && ResultInArray[index])
+                {
+                    count++;
+                }
+                else if (!ResultOutArray[index] && ResultInArray[index])
+                {
+                    
+                    StartCoroutine(ICreateImage(bytesList[index], index));
+                }
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Main END!!");
+        }
+    }
     async void CreateImage(string path,int index)
     {
         Stopwatch sw = new Stopwatch();
@@ -44,13 +94,17 @@ public class ImageRead : MonoBehaviour {
         task.Start();
         await task;
         UnityEngine.Debug.Log("Task " + index + " Result :" + task.Result.Length);
+        bytesList.Add(task.Result);
+        ResultInArray[index] = true;    
         UnityEngine.Debug.Log("Task " + index + " End!!!!!!");
-        StartCoroutine(ICreateImage(task.Result, index));
+
+        //StartCoroutine(ICreateImage(task.Result, index));
         sw.Stop();
         timetotal += sw.ElapsedMilliseconds;
         //totalsw.Elapsed = totalsw.Elapsed + sw.Elapsed;
         UnityEngine.Debug.Log("time total :: " + timetotal);
     }
+
     IEnumerator ICreateImage(byte[] bytes, int index)
     {
         Stopwatch sw = new Stopwatch();
@@ -62,8 +116,10 @@ public class ImageRead : MonoBehaviour {
         images[index].LoadImage(bytes);
         images[index].Apply();
         UnityEngine.Debug.Log("Create " + index + " CreateImage End!!!!!!");
+        ResultOutArray[index] = true;
+        //ResultInArray[index] = true;
         sw.Stop();
-        timetotal += sw.ElapsedMilliseconds;
+        //timetotal += sw.ElapsedMilliseconds;
         //totalsw.Elapsed = totalsw.Elapsed + sw.Elapsed;
         UnityEngine.Debug.Log("time total :: " + timetotal);
         yield return null;
@@ -75,7 +131,6 @@ public class ImageRead : MonoBehaviour {
         sw.Reset();
         sw.Start();
         byte[] bytes = File.ReadAllBytes(path);
-        //UnityEngine.Debug.Log(bytes.Length);
         sw.Stop();
         timetotal += sw.ElapsedMilliseconds;
         return bytes;
